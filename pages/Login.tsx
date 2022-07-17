a30dd1cd157ac0c64801047bc3b4e9e41ba80442
K-Kizuku <kotani.kizuku@gmail.com>
2022-07-17 18:06:55 +0900

    fix

--- pages/Login.tsx
+++ pages/Login.tsx
@@ -1,93 +1,88 @@
-import React, {useState} from 'react';
-import Link from 'next/link'
-import styled from 'styled-components';
-import axios from 'axios';
+import React, { useState } from "react";
+import Link from "next/link";
+import styled from "styled-components";
+import axios from "axios";
 
 const instance = axios.create({
-  baseURL: 'https://onaka-api.herokuapp.com/'
+  baseURL: "https://onaka-api.herokuapp.com/",
 });
 
 function Login() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] =useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const loginSend = (e) => {
     e.preventDefault();
-    console.log(email, password); 
-/* バックエンドに送る処理に変える */
+    console.log(email, password);
+    /* バックエンドに送る処理に変える */
   };
 
   const postFunc = () => {
     const loginUser = async () => {
       const postData = {
         Email: email,
-        Password: password
+        Password: password,
       };
-  
-      const jwt = await instance.post(
-        `/api/v1/users/signin`, postData
-      );
-      console.log(jwt.data);
-      localStorage.setItem('token',jwt.data.jwt);
-    }
 
-    try{
+      const jwt = await instance.post(`/api/v1/users/signin`, postData);
+      console.log(jwt.data);
+      localStorage.setItem("token", jwt.data.jwt);
+    };
+
+    try {
       loginUser();
-    }
-    catch(err){
+    } catch (err) {
       console.error(err);
     }
-  }
+  };
 
   return (
-    <div style={{ textAlign: 'center', marginTop: '2em'}}><h1>ログイン画面</h1>
+    <div style={{ textAlign: "center", marginTop: "2em" }}>
+      <h1>ログイン画面</h1>
       <div>
-
         <div>
           <label>
             <div>メールアドレス</div>
             <input
               name="email"
               type="email"
-              onChange={(e) => setEmail(e. target.value)}
+              onChange={(e) => setEmail(e.target.value)}
             />
           </label>
         </div>
 
         <div>
-         <label>
-            <div>パスワード</div> 
+          <label>
+            <div>パスワード</div>
             <input
               name="password"
               type="password"
               onChange={(e) => setPassword(e.target.value)}
-              />
+            />
           </label>
         </div>
 
-        <button  onClick={postFunc}>
-            <Link href="/">
-              <a>ログイン</a>
+        <button onClick={postFunc}>
+          <Link href="/">
+            <a>ログイン</a>
+          </Link>
+        </button>
+
+        <Enrole>
+          <button type="submit">
+            <Link href="/Signup">
+              <a>
+                <div>新規登録画面へ</div>
+              </a>
             </Link>
           </button>
-        
-          <Enrole>
-            <button type="submit">
-              <Link href="/Signup">
-                <a><div>新規登録画面へ</div></a>
-              </Link>
-            </button>
-          </Enrole>
-          
-
-     </div>
-   </div>
+        </Enrole>
+      </div>
+    </div>
   );
 }
 
 export default Login;
 
-
 const Enrole = styled.p`
-text-align: center;
-
+  text-align: center;
 `;
