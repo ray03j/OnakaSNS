46a71689f520b42bfdb4170d1e03cf02bc0ec181
K-Kizuku <kotani.kizuku@gmail.com>
2022-07-17 18:57:22 +0900

    fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,13 +14,12 @@ function Home() {
   useEffect(() => {
     const jwt = localStorage.getItem("token");
     async function fetchPostData() {
-      await instance
-        .get("/posts", {
-          headers: {
-            Authorization: `Bearer ${jwt}`,
-          },
-        })
-        .then((res) => setPosts(res.data));
+      const res = await instance.get("/posts", {
+        headers: {
+          Authorization: `Bearer ${jwt}`,
+        },
+      });
+      setPosts(res.data);
     }
     fetchPostData();
     console.log(posts);
